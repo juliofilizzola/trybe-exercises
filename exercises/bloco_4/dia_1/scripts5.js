3f701f07dc81ad6a085e2ed7cd5bbe5512fd9285
Filizzola <juliofilizzola@hotmail.com>
2021-03-24 11:45:50 -0300

    quinta atividade do bloco 4.1

---
+++ exercises/bloco_4/dia_1/scripts5.js
@@ -0,0 +1,23 @@
+// if = se
+// else = não
+
+let trybe = 17;
+
+if(trybe >=14 && trybe < 14.40){
+  console.log('EEEEEEESSSSQQQQUUUUUEEEENTTAAAA')
+  //codigo
+}
+else if (trybe >= 16.30 && trybe < 17.50) {
+  console.log('Aula Ao Vivo')
+  //codigo
+}
+else if (trybe >= 19.40 && trybe < 20) {
+  console.log('Fechamento')
+  // outra condição
+}
+else {
+  console.log('fora dos momentos síncronos.')
+  //codigo
+}
+
+
