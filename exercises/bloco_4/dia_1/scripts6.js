950d9ae7a47109ae85d02dfb64297c4a938d168b
Filizzola <juliofilizzola@hotmail.com>
2021-03-24 12:33:55 -0300

    sexta atividade do bloco 4.1

---
+++ exercises/bloco_4/dia_1/scripts6.js
@@ -0,0 +1,13 @@
+var statusP = 'aprovada'
+
+switch (statusP) {
+  case 'aprovada':
+    console.log('é isso ai!');
+    break;
+  case 'reprovada':
+    console.log('estude mais');
+    break;
+  default:
+    console.log('não se aplica')
+    
+}
