7b9892446595beed8b7f6a5dc2712dcd0c813a13
Filizzola <juliofilizzola@hotmail.com>
2021-03-24 10:57:21 -0300

    segunda atividade do bloco 4.1

---
+++ exercises/bloco_4/dia_1/scripts2.js
@@ -0,0 +1,17 @@
+const name = 'Julio Filizzola';
+const birthCity = 'Belo Horizonte';
+const birthYear = 1996;
+birthYear = 2020;
+birthCity = 'São Paulo'
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+
+console.log(birthYear)
+
+// O Erro está acontecendo, pois estamos tentando atribuir outro valor a uma constante.
+// Isso não pode pois uma constante possui aquele valor inicial ao qual cetamos e não pode 
+// ser mudado desse jeito. 
+// Agora se fosse um var, poderiamos mudar sem poreblemas.
