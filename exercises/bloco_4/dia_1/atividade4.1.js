3ff93f1e4e0bb90d85b023881015e725d6306024
Filizzola <juliofilizzola@hotmail.com>
2021-03-24 12:47:26 -0300

    segunda pratica

--- exercises/bloco_4/dia_1/atividade4.1.js
+++ exercises/bloco_4/dia_1/atividade4.1.js
@@ -1,5 +1,5 @@
 // primeira atividade.
-
+console.log('.........primeiro........')
 var a = 10;
 var b = 5;
 
@@ -8,3 +8,11 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+// segundo
+console.log('.........segundo........')
+
+var maxValor = Math.max(a, b)
+console.log(maxValor)
+
+
