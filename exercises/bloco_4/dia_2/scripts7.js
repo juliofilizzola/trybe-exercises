1eb8303ce37fdf2300095591580bad05a8990ab3
Filizzola <juliofilizzola@hotmail.com>
2021-03-25 10:08:41 -0300

    add arquivo scripts 7  e atividade 1

---
+++ exercises/bloco_4/dia_2/scripts7.js
@@ -0,0 +1,23 @@
+// // ARRAYS 
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList.length);
+// // 3
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// Brincar com o cachorro
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
