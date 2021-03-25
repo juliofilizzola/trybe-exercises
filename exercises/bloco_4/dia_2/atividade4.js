4059a37652922d284bf1e1188fa526a51d961c37
Filizzola <juliofilizzola@hotmail.com>
2021-03-25 12:44:25 -0300

    add atividade

--- exercises/bloco_4/dia_2/atividade4.js
+++ exercises/bloco_4/dia_2/atividade4.js
@@ -7,8 +7,15 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //2
 
-for (index = 0; index < numbers.length; index += 1){
- 
-  console.log(numbers[index]+ index)
+let soma = 0;
+
+// for (let i in numbers){
+//     soma += numbers[i]
+//     console.log(soma)
+// }
+for (let index =0; index <numbers.length; index += 1 ){
+  soma += numbers[index]
+  console.log(soma)
 }
-//
+
+//3
