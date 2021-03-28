f512d8b2ac591037f80690d717e81bc4f313775c
Filizzola <juliofilizzola@hotmail.com>
2021-03-27 21:30:43 -0300

    refaturando as atividade

--- exercises/bloco_4/dia_3/Atividade.js
+++ exercises/bloco_4/dia_3/Atividade.js
@@ -1,11 +1,19 @@
 //1
 
-let n = 2;
+let n = 5;
 let espaco = '';
 let quest = '*';
 
 for (let index = 0; index < n; index += 1){
-  espaco = espaco + quest
+  espaco = espaco + quest;
+  // console.log(espaco)
+
+
+  // if (index <= n){
+  //   console.log(espaco)
+  // } else {
+  //   console.log('cab')
+  // }
 };
 
 for (let index = 0; index < n; index += 1) {
@@ -30,13 +38,13 @@ for (let index = 0; index < n; index += 1) {
 // 3
 
 
-let size = 11;
+let size = 5;
 let symbol = '*';
 let inputline = '';
 let imputPosition = size;
 
 for(let index = 0; index <= size; index += 1){
-  for(let cindex = 0; cindex<= size; cindex += 1){
+  for(let cindex = 0; cindex < size; cindex += 1){
     if(cindex < imputPosition){
       inputline = inputline + ' ';
     } else {
