5e0e0443a9050665eb42ea4ef5c2564f421dd763
Filizzola <juliofilizzola@hotmail.com>
2021-03-28 21:38:26 -0300

    add arquivo de exemplos

---
+++ exercises/bloco_4/dia_4/exemplos.js
@@ -0,0 +1,17 @@
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  }
+};
+
+console.log('A jogadora '+ player.name + ' ' + player.lastName + ' ' + 'tem' + ' ' + player.age + ' ' + 'anos de idade.');
+
+player['bestInTheWorlds'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log('A jogadora '+ player.name + ' ' + player.lastName + ' ' + 'foi eleiita a melhor do mundo por 6 vezes, em ' + player.bestInTheWorlds);
+
+console.log('A jogadora '+ player.name + ' ' + player.lastName + ' ' +  'possui ' + player.medals.golden + ' de Ouro e '+ player.medals.silver + ' de prata.' );
