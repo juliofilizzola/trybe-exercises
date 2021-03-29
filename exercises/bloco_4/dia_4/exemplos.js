af8bb65cbc9a6a1df3c45f0c724ef237c4260175
Filizzola <juliofilizzola@hotmail.com>
2021-03-28 22:07:48 -0300

    add arquivo de exemplos

--- exercises/bloco_4/dia_4/exemplos.js
+++ exercises/bloco_4/dia_4/exemplos.js
@@ -1,3 +1,5 @@
+// exemplos de objeto.
+
 let player = {
   name: 'Marta',
   lastName: 'Silva',
@@ -8,6 +10,8 @@ let player = {
   }
 };
 
+// pratica
+
 console.log('A jogadora '+ player.name + ' ' + player.lastName + ' ' + 'tem' + ' ' + player.age + ' ' + 'anos de idade.');
 
 player['bestInTheWorlds'] = [2006, 2007, 2008, 2009, 2010, 2018];
@@ -15,3 +19,8 @@ player['bestInTheWorlds'] = [2006, 2007, 2008, 2009, 2010, 2018];
 console.log('A jogadora '+ player.name + ' ' + player.lastName + ' ' + 'foi eleiita a melhor do mundo por 6 vezes, em ' + player.bestInTheWorlds);
 
 console.log('A jogadora '+ player.name + ' ' + player.lastName + ' ' +  'possui ' + player.medals.golden + ' de Ouro e '+ player.medals.silver + ' de prata.' );
+
+
+// For in
+
+
