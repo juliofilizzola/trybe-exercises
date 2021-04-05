ab399f035fbcd9b1869390a6a4608de24ac93506
Filizzola <juliofilizzola@hotmail.com>
2021-04-05 18:43:33 -0300

    atividade

---
+++ exercises/bloco_5/dia_1/JAVASCRIPT/script3.js
@@ -0,0 +1,21 @@
+let header = document.getElementById('header-container');
+header.style.backgroundColor = 'green';
+let footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'green';
+
+let titu = document.querySelectorAll('.emergency-tasks h3');
+titu[0].style.backgroundColor = 'Magenta';
+let titu2 = document.querySelectorAll('.emergency-tasks h3');
+titu2[1].style.backgroundColor = 'magenta';
+
+let title = document.querySelectorAll('.no-emergency-tasks h3');
+title[0].style.backgroundColor = 'black';
+let title2 = document.querySelectorAll('.no-emergency-tasks h3');
+title2[1].style.backgroundColor = 'black';
+
+
+let bgClass1 = document.querySelectorAll('.emergency-tasks');
+bgClass1[0].style.backgroundColor = 'pink';
+
+let bgClass2 = document.querySelectorAll('.no-emergency-tasks');
+bgClass2[0].style.backgroundColor = 'yellow';
