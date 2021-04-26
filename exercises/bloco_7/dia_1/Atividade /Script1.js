10f7393d3d6152f3ee9d0ac359543d39d471690d
Filizzola <juliofilizzola@hotmail.com>
2021-04-26 17:35:43 -0300

    bloco 7.1 atividade 2

--- exercises/bloco_7/dia_1/Atividade /Script1.js
+++ exercises/bloco_7/dia_1/Atividade /Script1.js
@@ -15,4 +15,14 @@ testingScope(true);
 // atividade 1
 const testingScope1 =  escopo => escopo ?  'Não devo ser utilizada fora do meu escopo (if)' : 'Não devo ser utilizada fora meu escopo (else)';
 
-console.log(testingScope1(false))
+console.log(testingScope1(false));
+
+//atividade 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const printNumber = number => `Os numeros ${number.sort((a, b) => a - b)} se encontram de forma crescente`;
+
+console.log(oddsAndEvens);
+
+console.log(printNumber(oddsAndEvens));
