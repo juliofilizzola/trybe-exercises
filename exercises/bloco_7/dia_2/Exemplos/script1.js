791fd3c6c0aa742b698a3b644c3923e5a6ef773c
Julio Filizzola <juliofilizzola@hotmail.com>
2021-04-27 11:01:24 -0300

    bloco 7.2 atividade de fixação

---
+++ exercises/bloco_7/dia_2/Exemplos/script1.js
@@ -0,0 +1,38 @@
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+customer.lastname = 'Xablau';
+
+console.log(customer);
+
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+let newKey = 'lastName';
+const lastName = 'Ferreira';
+customer[newKey] = lastName;
+newKey = 'fullName';
+const fullName = `${customer.firstName} ${customer.lastName}`;
+customer[newKey] = fullName;
+console.log(customer);
+
+// atividade de fixação
+
+const car = {
+  marca: 'Fiat',
+};
+
+const addObject = (ob, key, val) => {
+  ob.key = val;
+  return car;
+}
+
+
+console.log(addObject(car, 'modelo', 'Palio'));
+
