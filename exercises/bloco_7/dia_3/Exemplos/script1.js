ba629243afd08801ff60d10ded8d6adae5c4d7c5
Julio Filizzola <juliofilizzola@hotmail.com>
2021-04-28 18:32:42 -0300

    Bloco 7.3 Teste unitários atividade 1

--- exercises/bloco_7/dia_3/Exemplos/script1.js
+++ exercises/bloco_7/dia_3/Exemplos/script1.js
@@ -1,4 +1,4 @@
-// const assert = require('assert');
+const assert = require('assert');
 
 // function add(a, b) {
 //   return a + b;
@@ -6,17 +6,18 @@
 
 // const expected = add(1, 5);
 
-// //assert.ok(expected === 3, 'Um mais dois é igual a três'); // Checa se o primeiro argumento é verdadeiro
+// assert.ok(expected === 3, 'Um mais dois é igual a três'); // Checa se o primeiro argumento é verdadeiro
 // assert.strictEqual(expected, 3, 'Um mais dois é igual a três'); // Checa se o primeiro e segundo argumentos são iguais em valor e tipo (===)
-// //assert.notStrictEqual(expected, 4, 'Um mais dois é igual a três (e não quatro!)'); // Checa se o primeiro e segundo argumentos são diferentes (!==)
+// assert.notStrictEqual(expected, 4, 'Um mais dois é igual a três (e não quatro!)'); // Checa se o primeiro e segundo argumentos são diferentes (!==)
 
 
-const assert = require('assert');
 
 function division(x, y) {
-if (y === 0) throw new Error('parameter y must not be 0');
+if (y === 0) throw new Error('Xablau')
 return x / y;
 }
 
 assert.strictEqual(division(10, 2), 5); // OK
-assert.throws(() => { division(10, 0); }, /^Error: parameter y must not be 0$/); // OK 
+assert.throws(() => { division(10, 2); }); // OK 
+
+// console.log(division(10, 0))
