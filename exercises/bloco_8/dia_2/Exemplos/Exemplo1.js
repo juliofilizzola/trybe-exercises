af4b76eb355ceb644eea1d68ba8e087174364d0d
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-03 06:26:12 -0300

    Bloco 8.2 Exemplos de estruturas HOF

---
+++ exercises/bloco_8/dia_2/Exemplos/Exemplo1.js
@@ -0,0 +1,20 @@
+const arrayOfValues = ['josé', 'xblau', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.some((element) => {
+  console.log('Cada elemento do array:', element);
+});
+
+// Cada elemento do array: josé
+// Cada elemento do array: 50
+// Cada elemento do array: 0.25
+// Cada elemento do array: { comida: 'Chocolate' }
+
+// outro Exemplos
+const meuArray = [2, 7, 8, 9];
+
+meuArray.forEach((elemento) => {
+  if (elemento % 2 === 0) {
+    console.log(`${elemento} é divísivel por 2!`);
+  } else {
+    console.log(`${elemento} Não é divísivel por 2!`);
+  }
+});
