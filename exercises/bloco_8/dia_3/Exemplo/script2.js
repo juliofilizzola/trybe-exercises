f13813cd520dacae440c6fe7961440652ce3a05b
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-04 12:30:10 -0300

    Bloco 8.3 exemplos de map

---
+++ exercises/bloco_8/dia_3/Exemplo/script2.js
@@ -0,0 +1,53 @@
+// const persons = [
+//   { firstName: 'Maria', lastName: 'Ferreira' },
+//   { firstName: 'João', lastName: 'Silva' },
+//   { firstName: 'Antonio', lastName: 'Cabral' },
+// ];
+
+// const fullNames = [];
+
+// for (let index = 0; index < persons.length; index += 1) {
+//   fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+// }
+
+// console.log(fullNames);
+
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = persons.map((person) => `${person.firstName} xablau ${person.lastName}`);
+
+console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+const number = [1, 2, 3, 4];
+
+const newArry = number.map((number) => number * 2);
+
+console.log(newArry)
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+    { [product]: listPrices[index] }
+  ));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
