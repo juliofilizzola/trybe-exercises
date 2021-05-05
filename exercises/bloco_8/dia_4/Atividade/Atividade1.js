f4afd002b046156e8a05d0901934f43eb16cd022
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-05 18:25:22 -0300

    Bloco 8.4 Refaturaçao

--- exercises/bloco_8/dia_4/Atividade/Atividade1.js
+++ exercises/bloco_8/dia_4/Atividade/Atividade1.js
@@ -7,7 +7,7 @@ const arrays = [
     [4, 5, 6],
 ];
 
-const flatten = () => arrays.reduce((acc, curr) => acc.concat(curr), []);
+const flatten = () => arrays.reduce((acc, curr) => acc.concat(curr));
 
 console.log(flatten());
 
