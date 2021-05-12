82b7ce891b45aa3df7d212070ec7100617e730ca
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-12 07:08:48 -0300

    Bloco 9.2 exemplo de Promises

---
+++ exercises/bloco_9/dia_2/Exemplos/script.js
@@ -0,0 +1,10 @@
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+  }
+  resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
+});
+
+// que massa o fato de não precisar chamar a função 
