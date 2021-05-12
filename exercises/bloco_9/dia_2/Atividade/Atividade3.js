2d27d87b4e402993ad30d3cace971a6e073031df
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-12 18:47:11 -0300

    Bloco 9.2 Atividade erro 3

--- exercises/bloco_9/dia_2/Atividade/Atividade3.js
+++ exercises/bloco_9/dia_2/Atividade/Atividade3.js
@@ -1,5 +1,5 @@
 const fetchPromise = () => {
-  const myPromise = new Promise((acerto, error) => { // FAZENDO UM PROMISES
+  const myPromise = new Promise((resolve, reject) => { // FAZENDO UM PROMISES
     const myArray = Array.from(
       { length: 10 },
       () =>  Math.floor(Math.random() * 50) + 1 // FAZEMOS UMA ARRAY ALEATORIAMENTE, COM UM TAMANHO DE 10 ITENS
@@ -7,12 +7,12 @@ const fetchPromise = () => {
     const sum = myArray.map(number => number * number)
     .reduce((number, acc) => number + acc, 0);// SOMAMOS ESSES NUMEROS AO QUADRADO E RETORNAMOS UM VALOR;
 
-    (sum < 8000) ? xablau(sum) : ishinua(); // FAZEMOS UMA COMPARAÇÃO, SE TRUE = CHECKED; FALSE = REJEITADO/ ERRO
+    (sum < 8000) ? resolve(sum) : reject(); // FAZEMOS UMA COMPARAÇÃO, SE TRUE = CHECKED; FALSE = REJEITADO/ ERRO
   });
 
   myPromise
-    // COM O RESOLVE, FAZEMOS UMA SOMA COM OS NUMEROS DESSA NOVA ARRAY.
-    .then(array => console.log('xablau deu certo ' + array))// DEPOIS SOMAREMOS A ARRAY E DEPOIS RETORNARIAMOS UMA SOMA.
+    .then(sum => [2, 3, 5, 10].map(number => sum / number))// COM O RESOLVE, FAZEMOS UMA SOMA COM OS NUMEROS DESSA NOVA ARRAY.
+    .then(array => array.reduce((number, acc) => number + acc, 0))// DEPOIS SOMAREMOS A ARRAY E DEPOIS RETORNARIAMOS UMA SOMA.
     .catch(() =>
       console.log('É mais de oito mil! Essa promise deve estar quebrada!') // REJECT (MENSAGEM DE ERRO.);
     );
