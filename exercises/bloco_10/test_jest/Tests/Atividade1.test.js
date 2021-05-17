aa5fae765417aed5e21b77dbeaf6395c62e6b706
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-17 09:49:13 -0300

    exercise/10.1 Atividade 1

--- exercises/bloco_10/test_jest/Tests/Atividade1.test.js
+++ exercises/bloco_10/test_jest/Tests/Atividade1.test.js
@@ -1,7 +1,19 @@
 const sum = require('../Atividade/Atividade1');
 
 describe('Atividade 1', () => {
-  it('Test a', () => {
-    
-  })
-})
+  it('Test A', () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+
+  it('Test B', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  it('Test C', () => {
+    expect(() => sum(4, '5')).toThrow();
+  });
+  
+  it('Test D', () => {
+    expect(() => sum(4, '5')).toThrowError(new Error('parameters must be numbers'));
+  });
+});
