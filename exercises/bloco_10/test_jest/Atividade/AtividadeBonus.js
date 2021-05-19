d2abe2be275dd42becd1f8655852e246b562e934
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-19 08:42:24 -0300

    exercise/10.1 Atividade Bônuspart 1

---
+++ exercises/bloco_10/test_jest/Atividade/AtividadeBonus.js
@@ -0,0 +1,51 @@
+// Dados
+const professionalBoard = [
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
+
+// Pesquisa
+const searchEmployee = (ids, detail) => {
+  const search = professionalBoard.filter(({id}) => ids === id)[0];
+  
+  if (!search) throw new Error('ID não foi achada!');
+  if (!search[detail]) throw new Error('Nada encotrado');
+  return search[detail];
+
+};
+
+console.log(searchEmployee('8579-6', 'lastName'));
