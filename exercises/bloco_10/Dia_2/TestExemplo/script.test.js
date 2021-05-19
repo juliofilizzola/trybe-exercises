0eebb4918584ae6e13d903fb0973c73d03ef3297
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-19 11:15:11 -0300

    exercises/10.2 first exemple

--- exercises/bloco_10/Dia_2/TestExemplo/script.test.js
+++ exercises/bloco_10/Dia_2/TestExemplo/script.test.js
@@ -1,9 +1,9 @@
-// test("Não deveria passar!", () => { // Falso positivo, por conta do setTimeout
-//   // setTimeout(() => {
-//     expect(10).toBe(5);
-//     console.log('Deveria falhar!')
-//   // }, 500);
-// });
+test("Não deveria passar!", () => { // Falso positivo, por conta do setTimeout
+  setTimeout(() => {
+    expect(10).toBe(5);
+    console.log('Deveria falhar!')
+  }, 500);
+});
 
 test("Não deveria passar!", done => { // por conta da função callback
   setTimeout(() => {
