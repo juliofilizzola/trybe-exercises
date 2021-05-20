6cd3f000f91f91679392eb0b6ca9d1b445cd05fb
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-20 11:35:41 -0300

    [exercises/10.2] - Atividade 2

--- Modulo_Fundamentos/bloco_10/Dia_2/Atividade/Tests/Atividade2.test.js
+++ Modulo_Fundamentos/bloco_10/Dia_2/Atividade/Tests/Atividade2.test.js
@@ -6,3 +6,8 @@ it( 'test de id', () => {
     expect(r).toBe('Mark');
   });
 });
+
+it('test de erro', () =>{
+  expect.assertions(1);
+  return getUserName('52').catch((error) => expect(error).toEqual({ error: 'User with 52 not found.' }))
+})
