b5b7a49d10363e236dc59d41548a7c8ddc8347ff
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-23 20:44:27 -0300

    [exercises/10.3] - aplinficando exemplo 2

--- Modulo_Fundamentos/bloco_10/Dia_3/Exemplos/test/script.test.js
+++ Modulo_Fundamentos/bloco_10/Dia_3/Exemplos/test/script.test.js
@@ -1,26 +1,25 @@
 const script = require('../src/script');
 
-describe('Teste"Falso positivo"', () => {
-  it('Função recebe true', () => {
-   expect(script.divisivelPorDois()).toBe(true); // Não garantimos o retorno certo!
-  });
-});
+// describe('Teste"Falso positivo"', () => {
+//   it('Função recebe true', () => {
+//    expect(script.divisivelPorDois()).toBe(true); // Não garantimos o retorno certo!
+//   });
+// });
 
 describe('Teste Mock', () => {
+  script.divisivelPorDois = jest.fn().mockReturnValue(true)
+                                    .mockReturnValueOnce(false);
   script.divisivelPorDois();
   it('Função é chamada', () => {
     expect(script.divisivelPorDois).toHaveBeenCalled();
   });
-  script.divisivelPorDois = jest.fn().mockReturnValue(true);
   it('Se a Função retorna true', () => {
     expect(script.divisivelPorDois()).toBe(true);
   });
 
 
+  it('Se a função retorna false', () => {
+    expect(script.divisivelPorDois()).toBe(false);
+  })
+
 });
-// describe('Teste de função false', () => {
-//   script.divisivelPorDois = jest.fn().mockReturnValue(false);
-//   it('Se a função retorna false', () => {
-//     expect(script.divisivelPorDois()).toBe(false);
-//   })
-// })
