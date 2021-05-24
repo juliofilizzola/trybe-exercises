3e25980ee70514360339b74bc7d8d72931c6c26d
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-24 12:51:44 -0300

    [exercises/11.1] - Atividade 1

---
+++ Modulo_front-End/Bloco_11/Dia_1/atividade/src/imput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class InputText extends React.Component {
+  render() {
+    return (
+      <input type='text' className='inputext'></input>
+    )
+  }
+}
+
+export default InputText;
--- Modulo_front-End/Bloco_11/Dia_1/atividade/src/index.js
+++ Modulo_front-End/Bloco_11/Dia_1/atividade/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import InputText from './imput'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <InputText/>
+    <App/>
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
