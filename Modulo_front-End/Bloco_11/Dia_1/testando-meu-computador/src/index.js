9961c91dbd7c7f0a8fa85e729db774730228e59e
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-24 09:49:02 -0300

    [exercises/11.1] - test React exemplos

--- Modulo_front-End/Bloco_11/Dia_1/testando-meu-computador/src/index.js
+++ Modulo_front-End/Bloco_11/Dia_1/testando-meu-computador/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const add = () => {
+  const addI = (
+    <div>
+      <p>Xablau!</p>
+    </div>
+  );
+  ReactDOM.render(addI, document.getElementById('xablau'));
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>São {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+add();
+setInterval(tick, 1000);
