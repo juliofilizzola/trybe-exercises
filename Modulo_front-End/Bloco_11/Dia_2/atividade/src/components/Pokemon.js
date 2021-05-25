b1eedff6d1f2c16d96be3edffb95bd7cf55350d1
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-25 13:24:57 -0300

    [exercises/11.2] - Atividade 1 (criando arquivo Pokemon)

---
+++ Modulo_front-End/Bloco_11/Dia_2/atividade/src/components/Pokemon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Pokemon extends React.Component {
+  render () {
+    // const { name, type, averageWeight, image } = this.props.pokemon;
+
+    return (
+      <div className="pokemon">
+        <div>
+          <p> {name} </p>
+          <p> {type} </p>
+          <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+        </div>
+      
+      </div>
+    );
+  }
+}
+
+
+
+export default Pokemon;
