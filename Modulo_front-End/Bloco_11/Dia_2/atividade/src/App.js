b91ff09107194c81a0e69c593010626ca75d9294
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-25 13:13:13 -0300

    [exercises/11.2] - Atividade 1 (Refeito do 0)

--- Modulo_front-End/Bloco_11/Dia_2/atividade/src/App.js
+++ Modulo_front-End/Bloco_11/Dia_2/atividade/src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Pokemon from './components/Pokemon';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Pokemon </h1>
-      <Pokemon pokemons={Data} />  
-    </div>
+
   );
 }
 
