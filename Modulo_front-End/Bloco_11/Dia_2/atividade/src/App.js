fdf44f06329db58ca2365b6a1ab47e0b0ae4e716
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-25 14:37:10 -0300

    [exercises/11.2] - Atividade 2 (Final Final)

--- Modulo_front-End/Bloco_11/Dia_2/atividade/src/App.js
+++ Modulo_front-End/Bloco_11/Dia_2/atividade/src/App.js
@@ -6,7 +6,7 @@ import Pokedex from './components/Pokedex';
 function App() {
   return (
     <div className="App">
-      <h1> Pokedex </h1>
+      <h1>My Pokedex! </h1>
       <p>Essa é minha coleção!</p>
       <Pokedex pokemons={Pokemons} />
     </div>
