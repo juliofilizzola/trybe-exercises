64ffd3b02c027ddae64656510bb0678499f3494b
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-25 12:58:12 -0300

    [exercises/11.2] - Atividade 1 (andamento)

--- Modulo_front-End/Bloco_11/Dia_2/atividade/src/components/Pokemon.js
+++ Modulo_front-End/Bloco_11/Dia_2/atividade/src/components/Pokemon.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
 
-
-export default class Pokemon extends Component {
+class Pokemon extends Component {
   render() {
-    const {im, alt} = this.props
-    return (
-      <div >
-        <img src={im} alt={alt} />;
-      </div>
+       return (
+     <div>
+       <div>
+        <h1></h1>
+        <h2></h2>
+        <h3></h3>
+       </div>
+       <img src={image} alt={`${name} sprite`} />
+     </div>
     )
   }
 }
 
- 
+Pokemon.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      measurementUnit: PropTypes.string,
+      value: PropTypes.number
+    }),
+    image: PropTypes.string,
+  }).isRequired,
+};
+
+export default Pokemon;
