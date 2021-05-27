8b3c6ac94914b535261a8ea6983ca0230cf12394
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-27 09:11:55 -0300

    [exercises/12.1] exemplos de this e bind

--- Modulo_front-End/Bloco_12/Dia_1/exemplo/src/components/Button.jsx
+++ Modulo_front-End/Bloco_12/Dia_1/exemplo/src/components/Button.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 function handleClick() {
   console.log('Clicou no botão!')
 }
-function handleClick2() {
-  console.log('XABLAU')
-}
+
 function handleClick3() {
   console.log('ACHOU')
 }
@@ -13,11 +11,22 @@ class button extends React.Component {
   /* Repare que, diferentemente do HTML, no
   JSX você associa uma função a um evento
   passando a própria função entre chaves `{}` */
+
+  constructor () {
+    super()
+    this.handleClick2 = this.handleClick2.bind(this)
+  }
+
+  handleClick2() {
+    console.log(this);
+  } 
+
   render() {
+   
     return (
       <>
       <button onClick={handleClick}>Meu botão</button>
-      <button onClick={handleClick2}>Meu botão 2</button>
+      <button onClick={this.handleClick2}>Meu botão 2</button>
       <button onClick={handleClick3}>Meu botão 3</button>
 
       </>
