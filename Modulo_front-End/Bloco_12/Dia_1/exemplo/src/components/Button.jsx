50c79f76f670f685e3056814ebd444e4ea27288d
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-27 08:57:38 -0300

    [exercises/12.1] exemplos de eventos no react passo 2

--- Modulo_front-End/Bloco_12/Dia_1/exemplo/src/components/Button.jsx
+++ Modulo_front-End/Bloco_12/Dia_1/exemplo/src/components/Button.jsx
@@ -3,13 +3,25 @@ import React from 'react';
 function handleClick() {
   console.log('Clicou no botão!')
 }
-
+function handleClick2() {
+  console.log('XABLAU')
+}
+function handleClick3() {
+  console.log('ACHOU')
+}
 class button extends React.Component {
   /* Repare que, diferentemente do HTML, no
   JSX você associa uma função a um evento
   passando a própria função entre chaves `{}` */
   render() {
-    return <button onClick={handleClick}>Meu botão</button>
+    return (
+      <>
+      <button onClick={handleClick}>Meu botão</button>
+      <button onClick={handleClick2}>Meu botão 2</button>
+      <button onClick={handleClick3}>Meu botão 3</button>
+
+      </>
+    ) 
   }
 }
 
