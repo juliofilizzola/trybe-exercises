aac18d3140eb7b27bfb9f0d59594955bbec21d70
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-28 08:42:53 -0300

    [exercises/12.2] - Criando forms

--- Modulo_front-End/Bloco_12/Dia_2/exemplo/src/components/Forms.js
+++ Modulo_front-End/Bloco_12/Dia_2/exemplo/src/components/Forms.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
 
+
 export default class extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      estadoFavorito: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+  
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     return (
       <div>
-        
+        <form className="form">
+          <label>
+            Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
+              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+          </label>
+          <input
+            type="number"
+            name="idade"
+            value={this.state.estadoFavorito} onChange={this.handleChange}
+          />
+          <input
+            type="checkbox"
+            name="vaiComparecer"
+            value={this.state.estadoFavorito} onChange={this.handleChange}
+          />
+        </form>
       </div>
     )
   }
