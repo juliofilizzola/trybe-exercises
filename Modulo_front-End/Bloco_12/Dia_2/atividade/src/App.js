11fe0e6986bac49718b7bbcf581b3582862367c5
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-30 15:05:51 -0300

    [exercises/12.2] creat Component e edit app.js

--- Modulo_front-End/Bloco_12/Dia_2/atividade/src/App.js
+++ Modulo_front-End/Bloco_12/Dia_2/atividade/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Fieldset from './Component/Fieldset';
 
 function App() {
   return (
-  
+      <Fieldset/>
   );
 }
 
---
+++ Modulo_front-End/Bloco_12/Dia_2/atividade/src/Component/Fieldset.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Fieldset extends Component {
+  render() {
+    return (
+      <fieldset>
+      
+      </fieldset>
+    )
+  }
+}
