ab49c81371ccd6a4e10c1c1819785d63e7461b66
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-31 07:23:43 -0300

    [exercises/12.2] edit arquivos (apagando fieldset)

--- Modulo_front-End/Bloco_12/Dia_2/atividade/src/App.js
+++ Modulo_front-End/Bloco_12/Dia_2/atividade/src/App.js
@@ -1,35 +1,50 @@
-
+import React from 'react';
 import Combo from './Component/Combo';
 import Input from './Component/Input';
 import States from './States';
 
 
-function App() {
+class App extends React.Component {
 
-  this.state = {
-    counterMouserEnter: false,
+  constructor() {
+    super();
+
+    this.state = {
+      nome: '',
+      endereco: '',
+      cpf: '',
+      city: '',
+      estado: '',
+      type: '',
+      email: '',
+      counterMouserEnter: false,
+    }
+
+    this.changeState = this.changeState.bind(this);
   }
 
-
-
-handleMouseEnter() {
+  handleMouseEnter() {
   if(this.state.counterMouserEnter === false) {
     alert('Preencha isso, seu arrombado!')
   }
-}
-  return (
-    <form>
-      <fieldset>
-        <legend>
-          Seus Dados:
-        </legend>
-        <Input/>
-        <Combo state={ States }/>
-      </fieldset>
+  }
 
+  render() {
+    return (
+      <form>
+        <fieldset>
+          <legend>
+            Seus Dados:
+          </legend>
+          <Input/>
+          <Combo state={ States }/>
+        </fieldset>
+  
+  
+      </form>
+    );
+  }
 
-    </form>
-  );
 }
 
 export default App;
