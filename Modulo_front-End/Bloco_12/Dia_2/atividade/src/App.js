f1edf7bd2dcde66cce603f74c9e53620b056d489
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-31 07:19:10 -0300

    [exercises/12.2] edit arquivos

--- Modulo_front-End/Bloco_12/Dia_2/atividade/src/App.js
+++ Modulo_front-End/Bloco_12/Dia_2/atividade/src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
-import Form from './Component/Form';
+
+import Combo from './Component/Combo';
+import Input from './Component/Input';
+import States from './States';
+
 
 function App() {
+
+  this.state = {
+    counterMouserEnter: false,
+  }
+
+
+
+handleMouseEnter() {
+  if(this.state.counterMouserEnter === false) {
+    alert('Preencha isso, seu arrombado!')
+  }
+}
   return (
-    <Form/>
+    <form>
+      <fieldset>
+        <legend>
+          Seus Dados:
+        </legend>
+        <Input/>
+        <Combo state={ States }/>
+      </fieldset>
+
+
+    </form>
   );
 }
 
--- Modulo_front-End/Bloco_12/Dia_2/atividade/src/Component/Fieldset.jsx
+++ Modulo_front-End/Bloco_12/Dia_2/atividade/src/Component/Fieldset.jsx
@@ -7,24 +7,12 @@ export default class Fieldset extends Component {
 constructor() {
   super();
 
-  this.state = {
-    counterMouserEnter: false,
-  }
-}
-
-
-handleMouseEnter() {
-  if(this.state.counterMouserEnter === false) {
-    alert('Preencha isso, seu arrombado!')
-  }
-}
+ 
   render() {
       const {func} = this.props;
     return (
       <fieldset>
-        <legend>
-          Seus Dados:
-        </legend>
+        
         <Input func={func}/>
         <Combo state={States} />
       </fieldset>
