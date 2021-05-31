4805a2cca4766a0f4997dbf295b40ab531a9dcd4
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-31 07:44:44 -0300

    [exercises/12.2] editando arquivo input.js

--- Modulo_front-End/Bloco_12/Dia_2/atividade/src/Component/Input.jsx
+++ Modulo_front-End/Bloco_12/Dia_2/atividade/src/Component/Input.jsx
@@ -7,32 +7,32 @@ export default class Input extends Component {
       <>
       <label>
         Seu Nome: 
-        <input name='nome' type="text" onChange={func} placeholder="Nome" required/>
+        <input name='nome' type="text" onChange={func} maxLength="40" placeholder="Nome" required/>
       </label> 
       <label>
         Seu E-mail: 
-        <input name='email' type="text" onChange={func} placeholder="E-Mail" required/>
+        <input name='email' type="text" onChange={func} maxLength="50" placeholder="E-Mail" required/>
       </label> 
       <label>
         Seu CPF: 
-        <input name='cpf' type="number" onChange={func} placeholder="CPF" required/>
+        <input name='cpf' type="number" onChange={func} maxLength="11" placeholder="CPF" required/>
       </label>
       <label>
         Seu Endereço: 
-        <input name='endereco' type="text" onChange={func} placeholder="Endereço" 
+        <input name='endereco' type="text" onChange={func} maxLength="200" placeholder="Endereço" 
         required/>
       </label>
       <label>
         Sua Cidade: 
-        <input name='city' type="text" onChange={func} placeholder="Cidade" required/>
+        <input name='city' type="text" onChange={func} maxLength="28" placeholder="Cidade" required/>
       </label>
       <label>
         Casa 
-        <input name='type' type="radio" onChange={func} value="casa" placeholder="Nome"required/>
+        <input name='type' type="radio" onChange={func} value="Casa" placeholder="Nome"required/>
       </label>
       <label>
         Apartamento 
-        <input name='type' type="Casa" onChange={func} value="Apartamento" placeholder="Nome" required/>
+        <input name='type' type="radio" onChange={func} value="Apartamento" placeholder="Nome" required/>
       </label>
       </>
     )
