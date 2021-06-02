13bb79daf9f8464921e81b6c2e767bb2e28ac0b6
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-02 08:50:58 -0300

    [exercises/13.1] - create function fetchjoke

--- Modulo_front-End/Bloco_13/Dia_1/exemplo/src/componentes/DataJoke.jsx
+++ Modulo_front-End/Bloco_13/Dia_1/exemplo/src/componentes/DataJoke.jsx
@@ -1,10 +1,51 @@
 import React, { Component } from 'react'
 
 export default class DataJoke extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      jokeObj: undefined,
+      loading: true,
+      storedJokes: [],
+    };
+  }
+
+  fetchJoke = async () => {
+    const requestHeaders = { headers: { Accept: 'application/json' } };
+    const requestReturn = await fetch('https://icanhazdadjoke.com/');
+    const requestObject = await requestReturn.json();
+
+    this.setState({
+      jokeObj: requestObject,
+    });
+  }
+
+  componentDidMount = () => {
+
+  }
+
+  saveJoke = () => {
+
+    this.fetchJoke();
+  }
+
+  renderJokeElement = () => {
     return (
       <div>
-        
+        <p>{this.setState.jokeObj.joke}</p>
+        <button type="button" onClick={this.saveJoke}>Salvar Piada!</button>
+      </div>
+    );
+  }
+
+  render() {
+    const { storedJokes } = this.state;
+    const loadingElement = <span>Loading...</span>;
+
+    return(
+      <div>
+        <span>{storedJokes.map(({ id, joke }) => (<p key{ id } > {joke} </p>))}</span>
+        <span> Renderização condiciional </span>
       </div>
     )
   }
