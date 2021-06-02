03bbfd9c0fc728b56c7c0ee5387f61093c2814d5
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-02 09:48:48 -0300

    [exercises/13.1] - colocando o loading no carregamento da nova piada

--- Modulo_front-End/Bloco_13/Dia_1/exemplo/src/componentes/DataJoke.jsx
+++ Modulo_front-End/Bloco_13/Dia_1/exemplo/src/componentes/DataJoke.jsx
@@ -11,13 +11,18 @@ export default class DataJoke extends Component {
   }
 
   fetchJoke = async () => {
-    const requestHeaders = { headers: { Accept: 'application/json' } };
-    const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders);
-    const requestObject = await requestReturn.json();
-    console.log(requestObject);
-    this.setState({
-      jokeObj: requestObject,
-    });
+    this.setState(
+      { loading: true }, async () => {
+        const requestHeaders = { headers: { Accept: 'application/json' } };
+        const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders);
+        const requestObject = await requestReturn.json();
+        console.log(requestObject);
+        this.setState({
+          loading: false,
+          jokeObj: requestObject,
+        });
+      }
+    );
   }
 
   componentDidMount = () => {
@@ -41,13 +46,13 @@ export default class DataJoke extends Component {
   }
 
   render() {
-    const { storedJokes, jokeObj } = this.state;
+    const { storedJokes, jokeObj, loading } = this.state;
     const loadingElement = <span>Loading...</span>;
 
     return(
       <div>
         <span>{storedJokes.map(({ id, joke }) => (<p key={ id } > {joke} </p>))}</span>
-        <span> {jokeObj ? this.renderJokeElement() : loadingElement} </span>
+        <span> {loading ? loadingElement : this.renderJokeElement()} </span>
       </div>
     )
   }
