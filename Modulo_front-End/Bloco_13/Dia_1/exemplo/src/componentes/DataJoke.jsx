bf08337dd01ad053bda69fdb2293345a9175fccb
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-02 09:41:39 -0300

    [exercises/13.1] - fecth e edit saveJoke

--- Modulo_front-End/Bloco_13/Dia_1/exemplo/src/componentes/DataJoke.jsx
+++ Modulo_front-End/Bloco_13/Dia_1/exemplo/src/componentes/DataJoke.jsx
@@ -14,38 +14,40 @@ export default class DataJoke extends Component {
     const requestHeaders = { headers: { Accept: 'application/json' } };
     const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders);
     const requestObject = await requestReturn.json();
-
+    console.log(requestObject);
     this.setState({
       jokeObj: requestObject,
     });
   }
 
   componentDidMount = () => {
-
+    this.fetchJoke();
   }
 
   saveJoke = () => {
-
+    this.setState(({ storedJokes, jokeObj}) => ({
+      storedJokes: [...storedJokes, jokeObj]
+    }))
     this.fetchJoke();
   }
 
   renderJokeElement = () => {
     return (
       <div>
-        <p>{this.setState.jokeObj.joke}</p>
+        <p>{this.state.jokeObj.joke}</p>
         <button type="button" onClick={this.saveJoke}>Salvar Piada!</button>
       </div>
     );
   }
 
   render() {
-    const { storedJokes } = this.state;
+    const { storedJokes, jokeObj } = this.state;
     const loadingElement = <span>Loading...</span>;
 
     return(
       <div>
         <span>{storedJokes.map(({ id, joke }) => (<p key={ id } > {joke} </p>))}</span>
-        <span> Renderização condiciional </span>
+        <span> {jokeObj ? this.renderJokeElement() : loadingElement} </span>
       </div>
     )
   }
