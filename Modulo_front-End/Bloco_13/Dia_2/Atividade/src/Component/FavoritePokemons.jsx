de68decb00fa4e85d982892f915883cbccc6eefc
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-07 19:14:49 -0300

    [exercises/13.2] - iniciando criação do componente favoritePokemons

---
+++ Modulo_front-End/Bloco_13/Dia_2/Atividade/src/Component/FavoritePokemons.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+export default FavoritePokemons = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
