8cd0c8c0c165aa45055b08d0f2ae4b3cbc6b856b
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-08 07:30:54 -0300

    [exercises/13.2] - edit lint

--- Modulo_front-End/Bloco_13/Dia_2/Atividade/src/Component/FavoritePokemons.jsx
+++ Modulo_front-End/Bloco_13/Dia_2/Atividade/src/Component/FavoritePokemons.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
 export default FavoritePokemons = () => {
   return (
     <div>
       
     </div>
-  )
+  );
 }
 
