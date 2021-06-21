d887806dc5ce362d82247ac116fa3848038d737c
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-21 14:55:06 -0300

    [exercises/15.1] - inicando tesst

---
+++ Modulo_front-End/Bloco_15/Dia_1/e-testes-react/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <label htmlFor="ip">
+          <input id='ip' type='text' />
+        </label>
+      </div>
+    )
+  }
+}
