6291c8c0683ee7a41147e0ad3dcf51a4038c8965
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-21 18:45:21 -0300

    [exercises/15.1] - comentando test

--- Modulo_front-End/Bloco_15/Dia_1/e-testes-react/src/App.test.js
+++ Modulo_front-End/Bloco_15/Dia_1/e-testes-react/src/App.test.js
@@ -7,5 +7,8 @@ test('verifica se há um input de email na tela', () => {
   const linkElement = getByLabelText(/email/i);
   // interagir com eles  (caso seja necessario.)
   expect(linkElement).toBeInTheDocument();
+  // verifica se é renderizado.
   expect(linkElement.type).toBe('text')
+  // verificando o tipo do item renderizado. 
+  
 });
