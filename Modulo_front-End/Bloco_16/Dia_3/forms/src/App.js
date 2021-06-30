d4de4d665a425f43a5f3533e55402ff6f1b553f9
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-30 14:47:50 -0300

    install redux

--- Modulo_front-End/Bloco_16/Dia_3/forms/src/App.js
+++ Modulo_front-End/Bloco_16/Dia_3/forms/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Provider } from 'react-redux';
 import FormsPerson from './components/FormsPerson';
 import FormsProfessional from './components/FormsProfessional';
 
 function App() {
   return (
     <div className="App">
+
       <FormsPerson />
       <FormsProfessional />
     </div>
