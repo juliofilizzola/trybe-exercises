6690589cecd54927679a1d1998a65e6fecc4a608
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-30 14:56:16 -0300

    add provider no index

--- Modulo_front-End/Bloco_16/Dia_3/forms/src/App.js
+++ Modulo_front-End/Bloco_16/Dia_3/forms/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import { Provider } from 'react-redux';
 import FormsPerson from './components/FormsPerson';
 import FormsProfessional from './components/FormsProfessional';
 
 function App() {
   return (
     <div className="App">
-
       <FormsPerson />
       <FormsProfessional />
     </div>
--- Modulo_front-End/Bloco_16/Dia_3/forms/src/index.js
+++ Modulo_front-End/Bloco_16/Dia_3/forms/src/index.js
@@ -1,3 +1,4 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,7 +6,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}  >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
